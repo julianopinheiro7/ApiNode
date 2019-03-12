5b0896d1431a7fe73d75f075ad88cdda0a2f8581
conlu_juliano <juliano@consorcioluiza.com.br>
2019-03-12 10:38:15 -0300

    Ajustes API
    Adicionei alguns ajustes na API

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const db = require('../config/dbconnect')();
 
 const app = express();
 const router = express.Router();
 
+//Conecta ao Banco
+db.dbconnect;
+
 //Carrega as Rotas
-const indexRoute = require('../routes/index-route');
-const productRoute = require('../routes/product-route');
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
